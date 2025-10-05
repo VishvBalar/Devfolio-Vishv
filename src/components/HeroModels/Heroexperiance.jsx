02d272db89e904fd9441a7fa7df5f5bda84be1e3
vishv balar <vishvbalar@gmail.com>
2025-10-06 00:37:03 +0530

    Hero Section Complete

--- src/components/HeroModels/Heroexperiance.jsx
+++ src/components/HeroModels/Heroexperiance.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { useMediaQuery } from 'react-responsive'
+import { Room } from './Room.jsx'
+import HeroLight from './HeroLight.jsx'
 
 const Heroexperiance = () => {
     const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45}}>
-        <ambientLight intensity={0.2} color="#1a1a40"/>
-        <directionalLight position={[5, 5, 5]} intensity={1}/>
-
         <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
@@ -19,10 +18,15 @@ const Heroexperiance = () => {
         minPolarAngle={Math.PI / 5}
         maxPolarAngle={Math.PI / 2}
         />
-        <mesh>
-            <boxGeometry args={[1,1,1]}/>
-            <meshStandardMaterial color="hsla(240, 24%, 70%, 1.00)"/>
-        </mesh>
+        <HeroLight/>
+        <group
+          scale={isMobile? 0.7 : 1}
+          position={[0, -3.5, 0]}
+          rotation={[0, -Math.PI / 4, 0]}
+        >
+            <Room/>
+        </group>
+        
     </Canvas>
   )
 }
